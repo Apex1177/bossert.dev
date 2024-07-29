2f1add02d86641ae0398582bdf5cd187d62dffb6
Apex1177 <119393751+Apex1177@users.noreply.github.com>
2024-07-28 22:49:22 -0500

    The website (there are def some bugs)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # bossert.dev
-my website
+This is my website project to showcase my projects and link people to my github and other socials.
+I learned a lot about JS, html, and CSS through this project.
+I plan to host download links to some of my other projects here in the future when they are ready. 
+I asked for permission to fork Jerrinth's website https://github.com/zugebot for most of the assets used in this website, it is very much appreciated. 
---
+++ script.js
@@ -0,0 +1,154 @@
+//below is the used splashes non-test
+const splashes = ["§1C§2o§3l§4o§5r§6m§7a§8t§9i§ac", "§kFUNKY LOL", "Home-Made!", "Déjà vu!", "Vote for net neutrality!", "90% bug free!", "Cloud computing!", "One of a kind!", "As seen on TV!","Awesome!","One of a kind!","Indev!","Check it out!", ".Dev!", "Turing-complete!", "Open source!"];
+
+//below is full list of minecraft splashes
+//const splashes = ["As seen on TV!","Awesome!","100% pure!","May contain nuts!","Better than Prey!","More polygons!","Sexy!","Limited edition!","Flashing letters!","Made by Notch!","It's here!","Best in class!","It's finished!","Kind of dragon free!","Excitement!","More than 500 sold!","One of a kind!","Heaps of hits on YouTube!","Indev!","Spiders everywhere!","Check it out!","Holy cow, man!","It's a game!","Made in Sweden!","Uses LWJGL!","Reticulating splines!","Minecraft!","Yaaay!","Singleplayer!","Keyboard compatible!","Undocumented!","Ingots!","Exploding creepers!","That's no moon!","l33t!","Create!","Survive!","Dungeon!","Exclusive!","The bee's knees!","Down with O.P.P.!","Closed source!","Classy!","Wow!","Not on steam!","Oh man!","Awesome community!","Pixels!","Teetsuuuuoooo!","Kaaneeeedaaaa!","Now with difficulty!","Enhanced!","90% bug free!","Pretty!","12 herbs and spices!","Fat free!","Absolutely no memes!","Free dental!","Ask your doctor!","Minors welcome!","Cloud computing!","Legal in Finland!","Hard to label!","Technically good!","Bringing home the bacon!","Indie!","GOTY!","Ceci n'est pas une title screen!","Euclidian!","Now in 3D!","Inspirational!","Herregud!","Complex cellular automata!","Yes, sir!","Played by cowboys!","OpenGL 1.2!","Thousands of colors!","Try it!","Age of Wonders is better!","Try the mushroom stew!","Sensational!","Hot tamale, hot hot tamale!","Play him off, keyboard cat!","Guaranteed!","Macroscopic!","Bring it on!","Random splash!","Call your mother!","Monster infighting!","Loved by millions!","Ultimate edition!","Freaky!","You've got a brand new key!","Water proof!","Uninflammable!","Whoa, dude!","All inclusive!","Tell your friends!","NP is not in P!","Notch <3 ez!","Music by C418!","Livestreamed!","Haunted!","Polynomial!","Terrestrial!","All is full of love!","Full of stars!","Scientific!","Cooler than Spock!","Collaborate and listen!","Never dig down!","Take frequent breaks!","Not linear!","Han shot first!","Nice to meet you!","Buckets of lava!","Ride the pig!","Larger than Earth!","sqrt(-1) love you!","Phobos anomaly!","Punching wood!","Falling off cliffs!","0% sugar!","150% hyperbole!","Synecdoche!","Let's danec!","Seecret Friday update!","Reference implementation!","Lewd with two dudes with food!","Kiss the sky!","20 GOTO 10!","Verlet intregration!","Peter Griffin!","Do not distribute!","Cogito ergo sum!","4815162342 lines of code!","A skeleton popped out!","The Work of Notch!","The sum of its parts!","BTAF used to be good!","I miss ADOM!","umop-apisdn!","OICU812!","Bring me Ray Cokes!","Finger-licking!","Thematic!","Pneumatic!","Sublime!","Octagonal!","Une baguette!","Gargamel plays it!","Rita is the new top dog!","SWM forever!","Representing Edsbyn!","Matt Damon!","Supercalifragilisticexpialidocious!","Consummate V's!","Cow Tools!","Double buffered!","Fan fiction!","Flaxkikare!","Jason! Jason! Jason!","Hotter than the sun!","Internet enabled!","Autonomous!","Engage!","Fantasy!","DRR! DRR! DRR!","Kick it root down!","Regional resources!","Woo, facepunch!","Woo, somethingawful!","Woo, /v/!","Woo, tigsource!","Woo, minecraftforum!","Woo, worldofminecraft!","Woo, reddit!","Woo, 2pp!","Google anlyticsed!","Now supports åäö!","Give us Gordon!","Tip your waiter!","Very fun!","12345 is a bad password!","Vote for net neutrality!","Lives in a pineapple under the sea!","MAP11 has two names!","Omnipotent!","Gasp!","...!","Bees, bees, bees, bees!","Jag känner en bot!","This text is hard to read if you play the game at the default resolution, but at 1080p it's fine!","Haha, LOL!","Hampsterdance!","Switches and ores!","Menger sponge!","idspispopd!","Eple (original edit)!","So fresh, so clean!","Slow acting portals!","Try the Nether!","Don't look directly at the bugs!","Oh, ok, Pigmen!","Finally with ladders!","Scary!","Play Minecraft, Watch Topgear, Get Pig!","Twittered about!","Jump up, jump up, and get down!","Joel is neat!","A riddle, wrapped in a mystery!","Huge tracts of land!","Welcome to your Doom!","Stay a while, stay forever!","Stay a while and listen!","Treatment for your rash!",'"Autological" is!',"Information wants to be free!",'"Almost never" is an interesting concept!',"Lots of truthiness!","The creeper is a spy!","Turing complete!","It's groundbreaking!","Let our battle's begin!","The sky is the limit!","Jeb has amazing hair!","Casual gaming!","Undefeated!","Kinda like Lemmings!","Follow the train, CJ!","Leveraging synergy!","This message will never appear on the splash screen, isn't that weird?","DungeonQuest is unfair!","110813!","90210!","Check out the far lands!","Tyrion would love it!","Also try VVVVVV!","Also try Super Meat Boy!","Also try Terraria!","Also try Mount And Blade!","Also try Project Zomboid!","Also try World of Goo!","Also try Limbo!","Also try Pixeljunk Shooter!","Also try Braid!","That's super!","Bread is pain!","Read more books!","Khaaaaaaaaan!","Less addictive than TV Tropes!","More addictive than lemonade!","Bigger than a bread box!","Millions of peaches!","Fnord!","This is my true form!","Totally forgot about Dre!","Don't bother with the clones!","Pumpkinhead!","Hobo humping slobo babe!","Made by Jeb!","Has an ending!","Finally complete!","Feature packed!","Boots with the fur!","Stop, hammertime!","Testificates!","Conventional!","Homeomorphic to a 3-sphere!","Doesn't avoid double negatives!","Place ALL the blocks!","Does barrel rolls!","Meeting expectations!","PC gaming since 1873!","Ghoughpteighbteau tchoghs!","Déjà vu!","Déjà vu!","Got your nose!","Haley loves Elan!","Afraid of the big, black bat!","Doesn't use the U-word!","Child's play!","See you next Friday or so!","From the streets of Södermalm!","150 bpm for 400000 minutes!","Technologic!","Funk soul brother!","My life for Aiur!","Lennart lennart = new Lennart();","I see your vocabulary has improved!","Who put it there?","You can't explain that!","if not ok then return end","§1C§2o§3l§4o§5r§6m§7a§8t§9i§ac","§kFUNKY LOL","SOPA means LOSER in Swedish"];
+//below is list of splashes for testing
+//const splashes = ["§1C§2o§3l§4o§5r§6m§7a§8t§9i§ac", "§kFUNKY LOL"]
+
+const pixel5 = ["A","B","C","D","E","F","G","H","J","K","L","M","N",'O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','g','h','j','m','n','o','p','q','r','s','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','#',"$","%","&","*","+","-","/",'<',"=",">","?","@","\\","^","_","~"]
+
+function randNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const span = document.getElementById('splashSpan');
+const root = document.querySelector(':root');
+root.style.setProperty('--text-width', '2vw');
+span.innerText = splashes[randNum(0, splashes.length-1)];
+
+function spamCharacters() {
+    let text = "";
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)] + " "
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)]
+    span.innerText = text
+
+    setTimeout(()=>{
+        spamCharacters()
+    },10)
+}
+
+const colorList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]; 
+const colors = ["#000000", "#0000AA", "#00AA00", "#00AAAA", "#AA0000", "#AA00AA", "#FFAA00", "#AAAAAA", "#555555", "#5555FF", "#55FF55", "#55FFFF", "#FF5555", "#FF55FF", "#FFFF55", "#FFFFFF"];
+const colorShadows = ["#000000", "#030222", "#032701", "#0a2d27", "#230507", "#240c22", "#242506", "#242506", "#181818", "#151345", "#0e3d0f", "#114340", "#491112", "#3d1839", "#504f50", "#595647"]
+
+function writeHTMLForCharacter(color, char) {
+    return `<span style="text-shadow:calc(var(--text-width)/10) calc(var(--text-width)/10) ${colorShadows[colorList.indexOf(color)]};color:${colors[colorList.indexOf(color)]}">${char}</span>`
+}
+
+function colorCharacters() {
+    let text = span.innerText
+    let newText = ""
+    let i = 1
+    newText += writeHTMLForCharacter(text[i], text[i+1]);
+    i += 3;
+    newText += writeHTMLForCharacter(text[i], text[i+1]);
+    i += 3;
+    newText += writeHTMLForCharacter(text[i], text[i+1]);
+    i += 3;
+    newText += writeHTMLForCharacter(text[i], text[i+1]);
+    i += 3;
+    newText += writeHTMLForCharacter(text[i], text[i+1]);
+    i += 3;
+    newText += writeHTMLForCharacter(text[i], text[i+1]);
+    i += 3;
+    newText += writeHTMLForCharacter(text[i], text[i+1]);
+    i += 3;
+    newText += writeHTMLForCharacter(text[i], text[i+1]);
+    i += 3;
+    newText += writeHTMLForCharacter(text[i], text[i+1]);
+    i += 3;
+    newText += writeHTMLForCharacter(text[i], text[i+1]);
+    span.innerHTML = newText
+
+}
+
+
+if (span.innerText.includes('§k')){
+    span.innerText = span.innerText.replaceAll("§k", "")
+    spamCharacters()
+} else if (span.innerText.includes('§1')){
+    colorCharacters()
+}
+
+function setFontSize() {
+    let width = 1100 / span.offsetWidth > 5 ? 5 : 1000 / span.offsetWidth;
+    root.style.setProperty('--text-width', width + 'vw');
+    span.style.opacity = 1
+    span.style.animationPlayState = "unset"
+}
+
+setFontSize()
+
+const hover = new Audio('./assets/audio/focus.wav');
+const click = new Audio('./assets/audio/press.wav');
+const back = new Audio('./assets/audio/back.wav');
+
+function spamCharacters() {
+    let text = "";
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)] + " "
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)]
+    text += pixel5[randNum(0, pixel5.length-1)]
+    span.innerText = text
+
+    setTimeout(()=>{
+        spamCharacters()
+    },10)
+}
+
+document.querySelectorAll("#titleScreen button").forEach((element)=>{
+    element.addEventListener("mouseover", (event) => {
+        const clone = hover.cloneNode(true);
+        clone.play();
+    });
+
+    element.addEventListener("mousedown", (event) => {
+        const clone = click.cloneNode(true);
+        clone.play();
+        document.getElementById('titleScreen').style.display = "none"
+        document.getElementById('menuScreen').style.display = "inherit"
+        loadMenu(element.innerText)
+    });
+})
+
+document.getElementById('back').addEventListener("mousedown", (event) => {
+    const clone = back.cloneNode(true);
+    clone.play();
+    document.getElementById('titleScreen').style.display = "inherit"
+    document.getElementById('menuScreen').style.display = "none"
+});
+
+socials = [["Discord", "https://discord.gg/yFEkA8kCJp"], ["LinkedIn", "https://www.linkedin.com/in/henry-bossert-86a28a320/"], ["Github", "https://github.com/Apex1177"]]
+projects = [["AutoMC", "https://github.com/Apex1177/AutoMC"], ["MapsNotIncluded", "https://github.com/Apex1177/MapsNotIncluded"]]
+info = [["Bio", "https://www.bossert.dev/bio.html"]]
+
+function loadMenu(name){
+    name=name.toLowerCase()
+    const array = this[name]
+    document.getElementById('menuButtons').innerHTML = ""
+    array.forEach((element)=>{
+        document.getElementById('menuButtons').innerHTML += `<a target="_blank" href="${element[1]}"><img src="./assets/icons/${name}/${element[0].toLowerCase()}.png"></img><span>${element[0]}</span></a><br>`
+    })
+}
+
+document.addEventListener("keydown", (event) => {
+    if (event.code == "Escape" && document.getElementById('menuScreen').style.display == "inherit") {
+        const clone = back.cloneNode(true);
+        clone.play();
+        document.getElementById('titleScreen').style.display = "inherit"
+        document.getElementById('menuScreen').style.display = "none"
+    }
+})
